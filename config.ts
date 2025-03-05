42e423171c430b508b3df8a4be4664804c002c18
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-05 21:14:53 +0300

    Add desktop support and Telegram detection

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  botUsername: 'your_bot_username', // Замените на имя вашего бота без @ 
+  telegramBotToken: '8181327273:AAE4Qwkg9OCMYaBCuYmJ_k-N25ZGuhaSOtg',
+  webAppUrl: 'https://mini-app-nft.vercel.app'
+}; 
